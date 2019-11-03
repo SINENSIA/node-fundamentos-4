36c5fa8d4e33ddf93a84384d8c60ebcf774a1369
Carlos Arranz XPS15 <carlos_arranz@yahoo.com>
2019-11-03 20:15:38 +0100

    Añadido dotenv

---
+++ dotenv-conexion.js
@@ -0,0 +1,16 @@
+const mongo = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Dotenv
+require('dotenv').config();
+
+const url = `mongodb://${process.env.MONGODB_USER}:${encodeURIComponent(process.env.MONGODB_PASS)}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+// Database Name
+const dbName = 'curso';
+
+const connection = mongo.connect(url);
+
+connection
+.then(() => console.log("Connected"))
+.catch((err) => console.log("Catch Se ha producido un error: " + err));
+
