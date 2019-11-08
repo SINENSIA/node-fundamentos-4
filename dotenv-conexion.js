088cc3ab80572417089e822e7b7ccedbe8d16554
Carlos Arranz XPS15 <carlos_arranz@yahoo.com>
2019-11-08 08:34:59 +0100

    conexión con db suando dotenv

--- dotenv-conexion.js
+++ dotenv-conexion.js
@@ -4,9 +4,6 @@ const assert = require('assert');
 // Dotenv
 require('dotenv').config();
 const url = `mongodb://${process.env.MONGODB_USER}:${encodeURIComponent(process.env.MONGODB_PASS)}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
-//const url = `mongodb://${process.env.MONGODB_USER}:${encodeURIComponent(process.env.MONGODB_PASS)}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
-// Database Name
-//const dbName = 'curso';
 
 const connection = mongo.connect(url);
 
